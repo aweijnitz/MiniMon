3688b6417cd7a065057182947afcd68553d31d14
Anders Weijnitz <anders.weijnitz@gmail.com>
2013-10-06 22:28:08 +0200

    First working version

---
+++ README.md
@@ -0,0 +1,15 @@
+#MiniMon
+A node.js server monitor app for the Rasberry Pi.
+
+# Installation
+(will be using npm)
+
+# Setup
+For now: copy minimon.js and roll your own
+
+# Running the tests
+
+# Motivation
+
+# Known issues
+
---
+++ minimon.js
@@ -0,0 +1,134 @@
+var os = require('os-utils');
+var RounderDB = require('rounderdb');
+var http = require('http');
+var util = require('util');
+var fs = require('fs');
+
+var selfMon = require('./lib/monitorSelf.js');
+var router = require('./lib/router.js');
+
+var config = require('./conf/default.conf');
+var DBconf = {
+    "persistenceConf": {
+        "dbFile": "./db/rounderDB.json",
+        "syncInterval": 5
+    },
+    "cpuUsage": [
+        {
+            "capacity": 60,
+            "aggregationStrategy": "average"
+        },
+        {
+            "capacity": 600,
+            "aggregationStrategy": "average"
+        }
+    ],
+    "loadAvg": [
+        {
+            "capacity": 60,
+            "aggregationStrategy": "average"
+        },
+        {
+            "capacity": 600,
+            "aggregationStrategy": "average"
+        }
+    ],
+    "selfMem": [
+        {
+            "capacity": 60,
+            "aggregationStrategy": "average"
+        },
+        {
+            "capacity": 600,
+            "aggregationStrategy": "average"
+        }
+    ],
+    "freemem": [
+        {
+            "capacity": 60,
+            "aggregationStrategy": "average"
+        },
+        {
+            "capacity": 600
+        }
+    ]
+};
+
+var port = 8000;
+var db = null;
+
+if (fs.existsSync(DBconf.persistenceConf.dbFile)) {
+    console.log("CREATING FROM FILE: " + DBconf.persistenceConf.dbFile);
+    var data = fs.readFileSync(DBconf.persistenceConf.dbFile);
+    var obj = JSON.parse(data);
+    db = RounderDB.loadInstance(obj);
+} else
+    db = RounderDB.createInstance(DBconf);
+
+// Enable logging
+var doLogging = false;
+var dryrun = false; // if true, don't add anything to db (just print, if doLogging is enabled)
+
+var intervalCPU = 1000;
+var intervalLoadAvg = 1 * 60 * 1000;
+var intervalMem = 1500;
+
+var addToDB = function (key, value) {
+    if (!dryrun)
+        db.add(key, value);
+}
+
+var cpuMon = function () {
+    os.cpuUsage(function (v) {
+        log('CPU Usage (%): ' + v);
+        addToDB('cpuUsage', v);
+    });
+};
+
+var memMon = function () {
+    var freeMem = os.freememPercentage();
+    log('Free mem (%): ' + freeMem);
+    addToDB('freemem', freeMem);
+};
+
+var selfMem = function () {
+    var freeMem = selfMon.RAMUsed();
+    log('Process mem (MB): ' + freeMem);
+    addToDB('selfMem', freeMem);
+};
+
+
+var loadAvg = function () {
+    var load = os.loadavg(1);
+    log('Load Avg. : ' + load);
+    addToDB('loadAvg', load);
+};
+
+// Register data collectors
+//
+var cpuHandle = setInterval(cpuMon, intervalCPU);
+var cpuLoad = setInterval(loadAvg, intervalLoadAvg);
+var memHandle = setInterval(memMon, intervalMem);
+var selfMemHandle = setInterval(selfMem, intervalMem);
+
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+//    console.log("----- NEW REQEST!\n"+util.inspect(request));
+    console.log("----- NEW REQEST! method: " + request.method + " URL: " + request.url);
+    var result = router.dispatch(db, request.method, request.url);
+    response.writeHead(result.status, {"Content-Type": "text/plain"});
+    response.end(result.data);
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(port);
+
+console.log("Server running at http://127.0.0.1:8000/");
+
+
+var log = function (msg) {
+    if (doLogging)
+        console.log(msg);
+
+}
